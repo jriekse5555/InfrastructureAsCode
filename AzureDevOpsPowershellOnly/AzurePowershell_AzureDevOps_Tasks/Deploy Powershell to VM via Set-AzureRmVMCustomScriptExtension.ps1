5fdb13b0fc1b0aa516862a0a8e8589eea9668e2e
Jim Riekse <jiriekse@microsoft.com>
2018-12-29 23:16:23 -0500

    Added Comments

--- AzureDevOpsPowershellOnly/AzurePowershell_AzureDevOps_Tasks/Deploy Powershell to VM via Set-AzureRmVMCustomScriptExtension.ps1
+++ AzureDevOpsPowershellOnly/AzurePowershell_AzureDevOps_Tasks/Deploy Powershell to VM via Set-AzureRmVMCustomScriptExtension.ps1
@@ -1,3 +1,6 @@
+#Provides an example of running a powershell script on a VM using Set-AzureRmVMCustomScriptExtension
+#Sets up the path to the powershell script leveraging DevOps variables
 $FilePath = "$(artifactsLocation)" + "/Code/Powershell/test.ps1" + "$(artifactsLocationSasToken)"
 
-Set-AzureRmVMCustomScriptExtension -FileURI $FilePath -Run "Code\Powershell\test.ps1" -ResourceGroupName "$(ResourceGroup)" -VMName "$(VMName)"   -Location "$(Location)" -Name "Custom"
+#Sets the custom extension with the name of the powershell script (with blob prefix), and a name for the exteionsion. Note that this command can be used repeatably if the extension name is kept the same.
+Set-AzureRmVMCustomScriptExtension -FileURI $FilePath -Run "Code\Powershell\test.ps1" -ResourceGroupName "$(ResourceGroup)" -VMName "$(VMName)" -Location "$(Location)" -Name "Custom"
