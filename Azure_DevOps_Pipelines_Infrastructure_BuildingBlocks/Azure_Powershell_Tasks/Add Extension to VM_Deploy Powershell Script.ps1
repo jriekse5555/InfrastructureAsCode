32c7e5729bc7f7c87df130b20b218d0881452145
Jim Riekse <jiriekse@microsoft.com>
2019-01-21 20:14:42 -0500

    Updated script to use default extension

--- Azure_DevOps_Pipelines_Infrastructure_BuildingBlocks/Azure_Powershell_Tasks/Add Extension to VM_Deploy Powershell Script.ps1
+++ Azure_DevOps_Pipelines_Infrastructure_BuildingBlocks/Azure_Powershell_Tasks/Add Extension to VM_Deploy Powershell Script.ps1
@@ -7,4 +7,4 @@ $FilePath = "$(artifactsLocation)" + "/test.ps1" + "$(artifactsLocationSasToken)
 
 #Sets the custom extension with the name of the powershell script (with blob prefix), and a name for the exteionsion. Note that this command can be used repeatably if the extension name is kept the same.
 #Note that if the .ps1 file is not of the root of the container the blob prefix would be in the path such as: "BlobPrefix\test.ps1"
-Set-AzureRmVMCustomScriptExtension -FileURI $FilePath -Run "test.ps1" -ResourceGroupName "$(ResourceGroup)" -VMName "$(VMName)" -Location "$(Location)" -Name "Custom"
+Set-AzureRmVMCustomScriptExtension -FileURI $FilePath -Run "test.ps1" -ResourceGroupName "$(ResourceGroup)" -VMName "$(VMName)" -Location "$(Location)" -Name "CustomScriptExtension"
